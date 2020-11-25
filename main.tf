dd7fa7fffee4e6c0c57621eba5eb5e83ef4f479b
Chad Crowell <chad.crowell@linuxacademy.com>
2020-11-25 13:21:31 -0600

    change workflow

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+provider "azurerm" {
+  version = "2.35.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "remotestate-rg"
+    storage_account_name = "tfchazuregithubactions"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "resource_gp" {
   name     = "app-rg"
   location = var.location
